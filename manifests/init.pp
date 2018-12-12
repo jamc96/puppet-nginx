c947c216db35985590b768a1520a75a44f627e10
jamc96 <jamc.mejia@gmail.com>
2018-12-12 14:59:35 -0600

    using boolean type to validate proxy configurations state

--- manifests/init.pp
+++ manifests/init.pp
@@ -101,7 +101,7 @@ class nginx (
   Integer $names_hash_bucket_size                            = 64,
   Integer $names_hash_max_size                               = 512,
   $nginx_cfg_prepend                                         = false,
-  Enum['on', 'off'] $proxy                                   = 'on',
+  Boolean $proxy                                             = true,
   String $proxy_buffers                                      = '32 4k',
   String $proxy_buffer_size                                  = '8k',
   String $proxy_cache_inactive                               = '20m',
